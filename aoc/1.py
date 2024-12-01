140fa77eddee6d4f5b66779a5b9b6235c76b9050
Caspar Nettelbladt <caspar.n@gmail.com>
2024-12-01 20:13:56 +0100

    Add solution for Advent of Code Day 1 Part 2

--- aoc/1.py
+++ aoc/1.py
@@ -1,16 +1,8 @@
 import sys
 
 
-# https://adventofcode.com/2024/day/1#part2
-def main():
-    # Read from stdin
-    l_values = []
-    r_values = []
-    for line in sys.stdin:
-        # Split the lines into separate lists
-        values = line.strip().split()
-        l_values.append(int(values[0]))
-        r_values.append(int(values[1]))
+def part_1(l_values, r_values):
+    # https://adventofcode.com/2024/day/1#part1
     # Sort the lists
     l_values.sort()
     r_values.sort()
@@ -21,6 +13,31 @@ def main():
         distance = abs(l_value - r_value)
         sum_distance += distance
     print(sum_distance)
+
+
+def part_2(l_values, r_values):
+    # https://adventofcode.com/2024/day/1#part2
+    r_values_counts = {}
+    similarity_score = 0
+    for value in l_values:
+        # Count the number of times the value appears in the r_values
+        if value not in r_values_counts:
+            r_values_counts[value] = r_values.count(value)
+        similarity_score += value * r_values_counts[value]
+    print(similarity_score)
+
+
+def main():
+    # Read from stdin
+    l_values = []
+    r_values = []
+    for line in sys.stdin:
+        # Split the lines into separate lists
+        values = line.strip().split()
+        l_values.append(int(values[0]))
+        r_values.append(int(values[1]))
+
+    part_2(l_values, r_values)
 
 
 if __name__ == "__main__":
