0c0f264274b319f005431901bb1f3b3e40871ff3
Caspar Nettelbladt <caspar.n@gmail.com>
2024-12-02 18:13:17 +0100

    Add alt solution to part_1

--- aoc/2.py
+++ aoc/2.py
@@ -15,6 +15,18 @@ def part_1(reports):
     print(safe_count)
 
 
+def part_1_alt(reports):
+    safe_count = 0
+    for levels in reports:
+        distances = [x - y for x, y in zip(levels, levels[1:])]
+        if all(abs(x) in [1, 2, 3] for x in distances):
+            if all(x > 0 for x in distances):
+                safe_count += 1
+            if all(x < 0 for x in distances):
+                safe_count += 1
+    print(safe_count)
+
+
 def main():
     # Read from stdin
     # https://adventofcode.com/2024/day/2/input
